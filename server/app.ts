1f438618075704ea93f27c9089e6e7191432ea2b
Saawn <itissaavn@gmail.com>
2024-09-24 21:16:14 +0530

    Refactor app.ts to serve static files from the frontend/dist folder

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,15 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expenseRoute } from "./routes/expenses";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
 app.use(logger());
 
-app.get("/", (c) => {
-  return c.json({ message: "Hello, World yay!" });
-});
-
 app.route("/api/expenses", expenseRoute);
+
+app.get("*", serveStatic({ root: "./frontend/dist" }));
+app.get("*", serveStatic({ root: "./frontend/dist/index.html" }));
 
 export default app; // for Cloudflare Workers or Bun
