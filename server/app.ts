fec6c944af8b5118570ab495226936be578b6575
Saawn <itissaavn@gmail.com>
2024-09-29 21:52:50 +0530

    add authentication with kinde

--- server/app.ts
+++ server/app.ts
@@ -2,12 +2,16 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expenseRoute } from "./routes/expenses";
 import { serveStatic } from "hono/bun";
+import { authRoute } from "./routes/auth";
 
 const app = new Hono();
 
 app.use(logger());
 
-const apiRoutes = app.basePath("/api").route("/expenses", expenseRoute);
+const apiRoutes = app
+  .basePath("/api")
+  .route("/expenses", expenseRoute)
+  .route("/", authRoute);
 
 app.get("*", serveStatic({ root: "./frontend/dist" }));
 app.get("*", serveStatic({ root: "./frontend/dist/index.html" }));
